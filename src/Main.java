ab817af28a840ed18f1f9dbab0a744f62630e417
Александр <perchatki79@gmail.com>
2022-12-22 13:30:09 +0300

    homework3

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ public class Main {
         task3();
         task4();
         task5();
+        task6();
+        task7();
+        task8();
     }
     public static void task1 () {
         System.out.println("Задача 1");
@@ -61,6 +64,54 @@ public static void task4 () {
         int e = c * b;//всего коричневой
         System.out.println("В школе, где " + c + " классов, нужно " + d + " банок белой краски и " + e + " банок коричневой краски");
     }
+    public static void task6 () {
+        System.out.println("Задача 6");
+        int banan = 5;  //всего бананов
+        int weightBanan  = 80; // вес 1 банана
+        int milk = 200;// молоко
+        int weightMilk = 105/100; // вес 1мл молока
+        int icecream=2;//всего брикетов мороженого
+        int weightIcecream=100;//вес 1 брикета мороженого
+        int egg=4;//всего яиц
+        int weightEgg=70;//вес 1 яйца
+        int menu=banan*weightBanan+milk*weightMilk+icecream*weightIcecream+egg*weightEgg;
+        float kilo=menu/1000f;//перевод в кг
+        System.out.println("Завтрак спортсмена весит: " +menu+ " гр ("+ kilo+ " кг)");
+    }
+    public static void task7 () {
+        System.out.println("Задача 7");
+        int weightKg = 7;//вес в кг, сколько нужно сбросить
+        int weightGr = 7 * 1000;//вес в гр
+        int down1 = 250;//сброс веса каждый день по 250гр
+        int down2 = 500;//сброс веса каждый день по 500гр
+        System.out.println("Если спортсмен будет сбрасывать каждый день по " + down1 + "гр, то для снижения веса на 7 кг ему понадобится " + weightGr/down1 + " дней");
+        System.out.println("Если спортсмен будет сбрасывать каждый день по " + down2 + "гр, то для снижения веса на 7 кг ему понадобится " + weightGr/down2 + " дней");
+
+    }public static void task8 () {
+        System.out.println("Задача 8");
+        int salaryMasha = 67760;//ЗП Маши
+        int salaryDenis = 83690;//ЗП Дениса
+        int salaryKristina = 76230;//ЗП Кристины
+        int yearSalaryMasha=salaryMasha*12;
+        int yearSalaryDenis=salaryDenis*12;
+        int yearSalaryKristina=salaryKristina*12;
+        float newSalaryMasha = salaryMasha*1.1f;//новая ЗП Маши
+        float newSalaryDenis = salaryDenis*1.1f;//новая ЗП Дениса
+        float newSalaryKristina = salaryKristina*1.1f;//новая ЗП Кристины
+        float newYearSalaryMasha=newSalaryMasha*12f;
+        float newYearSalaryDenis=newSalaryDenis*12f;
+        float newYearSalaryKristina=newSalaryKristina*12f;
+        float Masha=newYearSalaryMasha-yearSalaryMasha;
+        float Denis=newYearSalaryDenis-yearSalaryDenis;
+        float Kristina=newYearSalaryKristina-yearSalaryKristina;
+        System.out.println("Маша теперь получает "+newSalaryMasha+" рублей. Годовой доход вырос на "+Masha+" рублей");
+        System.out.println("Денис теперь получает "+newSalaryDenis+" рублей. Годовой доход вырос на "+Denis+" рублей");
+        System.out.println("Кристина теперь получает "+newSalaryKristina+" рублей. Годовой доход вырос на "+Kristina+" рублей");
+
+
+    }
+
+
 
         }
 
